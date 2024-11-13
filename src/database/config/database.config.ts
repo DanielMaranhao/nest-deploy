572fe347df9feaf32a9467be345da2aebced9895
danielmaranhao <danielmarquesmaranhao@gmail.com>
2024-11-13 17:06:30 -0300

    Using certificate authority for database connection in aws

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,13 +1,20 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { readFileSync } from 'fs';
 
 export default registerAs('database', () => {
   const url = `postgresql://${process.env.DATASOURCE_USERNAME}:${process.env.DATASOURCE_PASSWORD}@${process.env.DATASOURCE_HOST}:${process.env.DATASOURCE_PORT}/${process.env.DATASOURCE_DATABASE}`;
+
+  const caPath = 'sa-east-1-bundle.pem';
+  const ca = readFileSync(caPath, 'utf8');
 
   const config = {
     type: 'postgres',
     url,
     autoLoadEntities: true,
+    ssl: {
+      ca,
+    },
   } as const satisfies TypeOrmModuleOptions;
   return config;
 });
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,21 @@
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
+import { readFileSync } from 'fs';
 import { DataSource } from 'typeorm';
 
 dotenvExpand.expand(dotenv.config());
 
 const url = `postgresql://${process.env.DATASOURCE_USERNAME}:${process.env.DATASOURCE_PASSWORD}@${process.env.DATASOURCE_HOST}:${process.env.DATASOURCE_PORT}/${process.env.DATASOURCE_DATABASE}`;
 
+const caPath = 'sa-east-1-bundle.pem';
+const ca = readFileSync(caPath, 'utf8');
+
 export default new DataSource({
   type: 'postgres',
   url,
+  ssl: {
+    ca,
+  },
   entities: ['dist/domain/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 });
